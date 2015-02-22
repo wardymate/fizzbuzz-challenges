18f12cd70643ad8a563a4d352d0e6a34fd22fc82
Chris Ward <chriswarddev@gmail.com>
2015-02-22 22:34:42 +0000

    finished fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -9,6 +9,14 @@ class Fizzbuzz
   end
 
   def is_divisible_by_fifteen(number)
-    true
+    number % 15 == 0
   end
+
+  def fizzing(number)
+    return "fizzbuzz" if is_divisible_by_fifteen(number)
+    return "fizz" if is_divisible_by_three(number)
+    return "buzz" if is_divisible_by_five(number)
+    number
+  end
+
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -24,4 +24,24 @@ describe Fizzbuzz do
     expect(fizzer.is_divisible_by_fifteen(15)).to eq true
   end
 
+  it 'shoulc know if a number is not divisible by fifteen' do
+    expect(fizzer.is_divisible_by_fifteen(2)).to eq false
+  end
+
+  it 'should return fizz if it is divisible by three' do
+    expect(fizzer.fizzing(3)).to eq "fizz"
+  end
+
+  it 'should return buzz if it is divisible by five' do
+    expect(fizzer.fizzing(5)).to eq "buzz"
+  end
+
+  it 'should return fizzbuzz if is is divisible by fifteen' do
+    expect(fizzer.fizzing(15)).to eq "fizzbuzz"
+  end
+
+  it 'should return number if not divisible by three or five' do
+    expect(fizzer.fizzing(1)).to eq 1
+  end
+
 end
